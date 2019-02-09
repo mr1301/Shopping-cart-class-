c129fd78318c95a44530db29af678a4ccc1666c2
mr1301 <mr1301@georgetown.edu>
2019-02-09 14:03:23 -0500

    shopping-cart csv expansion

--- shopping-cart.py
+++ shopping-cart.py
@@ -1,4 +1,28 @@
 import datetime
+import csv
+
+products =[]
+csv_file_path = "products.csv"  # a relative filepath
+
+with open(csv_file_path, "r") as csv_file:  # "r" means "open the file for reading"
+    reader = csv.DictReader(csv_file)  # assuming your CSV has headers
+    # reader = csv.reader(csv_file) # if your CSV doesn't have headers
+    for row in reader:
+        #d = dict(row)
+        d = {"id": row["id"], "name": row["name"], "price": float(row["price"])}
+        #print(type(d), d["name"], d["price"])
+        products.append(d)
+
+
+
+
+
+
+
+
+
+
+
 
 today = datetime.date.today()
 #print(str(today))  # > '2017-07-02'
@@ -10,49 +34,33 @@ now = datetime.datetime.now()
 
 #print("CHECK OUT AT", now.strftime("%Y-%m-%d"), now.strftime("%H:%M:%S"), "AM")
 
+
+
 #**************** Above = Time Module, Below = Inputs*******************
-products = [
-    {"id": 1, "name": "Chocolate Sandwich Cookies",
-        "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id": 2, "name": "All-Seasons Salt", "department": "pantry",
-        "aisle": "spices seasonings", "price": 4.99},
-    {"id": 3, "name": "Robust Golden Unsweetened Oolong Tea",
-        "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id": 4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce",
-        "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id": 5, "name": "Green Chile Anytime Sauce", "department": "pantry",
-        "aisle": "marinades meat preparation", "price": 7.99},
-    {"id": 6, "name": "Dry Nose Oil", "department": "personal care",
-        "aisle": "cold flu allergy", "price": 21.99},
-    {"id": 7, "name": "Pure Coconut Water With Orange",
-        "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id": 8, "name": "Cut Russet Potatoes Steam N' Mash",
-        "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id": 9, "name": "Light Strawberry Blueberry Yogurt",
-        "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id": 10, "name": "Sparkling Orange Juice & Prickly Pear Beverage",
-        "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id": 11, "name": "Peach Mango Juice", "department": "beverages",
-        "aisle": "refrigerated", "price": 1.99},
-    {"id": 12, "name": "Chocolate Fudge Layer Cake",
-        "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id": 13, "name": "Saline Nasal Mist", "department": "personal care",
-        "aisle": "cold flu allergy", "price": 16.00},
-    {"id": 14, "name": "Fresh Scent Dishwasher Cleaner",
-        "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id": 15, "name": "Overnight Diapers Size 6",
-        "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id": 16, "name": "Mint Chocolate Flavored Syrup",
-        "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id": 17, "name": "Rendered Duck Fat", "department": "meat seafood",
-        "aisle": "poultry counter", "price": 9.99},
-    {"id": 18, "name": "Pizza for One Suprema Frozen Pizza",
-        "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id": 19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend",
-        "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id": 20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink",
-        "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-]  # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+
+
+#TODO read csv file
+  # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # TODO: write some Python code here to produce the desired functionality...
 #print(products)
@@ -143,3 +151,5 @@ print("TOTAL-PAY: ${0:.2f}".format(Total_Pay))
 print("-----------------------------")
 print("THANK YOU,", "SEE YOU AGAIN SOON")
 print("-----------------------------")
+
+#TODO read from csv.file
